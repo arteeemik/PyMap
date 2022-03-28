7aef0e0b248d73ccc02f5de0c6bced80460da6be
Artem <arteeemik@gmail.com>
2022-03-28 13:18:16 +0300

    fix comments: add enum classes

--- open-street-map/graph_class.py
+++ open-street-map/graph_class.py
@@ -1,10 +1,29 @@
 """The file describes the graph class."""
 import typing
+import enum
 
 import osmnx as ox
 from osmnx import distance as osmnx_distance
 from osmnx import utils_graph
 import networkx as nx
+
+
+class MovementType(enum.Enum):
+    """Тип способа передвижения для задания графа области."""
+
+    BIKE: str = "bike"
+    DRIVE: str = "drive"
+    WALK: str = "walk"
+    DRIVE_SERVICE: str = "drive_service"
+    ALL: str = "all"
+    ALL_PRIVATE: str = "all_private"
+
+
+class MetricaType(enum.Enum):
+    """Тип метрики, по которой будет считаться оптимальный маршрут."""
+
+    LENGTH: str = "length"
+    TIME: str = "time"
 
 
 class Graph:
@@ -16,7 +35,11 @@ class Graph:
     graph : networkx.MultiDiGraph
     """
 
-    def __init__(self, place_full_name: str, movement_type: str = "walk"):
+    def __init__(
+        self,
+        place_full_name: str,
+        movement_type: MovementType = MovementType.WALK,
+    ):
         """
         Определяет объект graph в классе.
 
@@ -25,12 +48,12 @@ class Graph:
         place_full_name : str
             Полное имя области, для которой хотим создать граф.
             example: "San Francisco, California, United States"
-        movement_type : str {"all_private", "all", "bike", "drive", "drive_service", "walk"}
+        movement_type : MovementType
             Тип передвижения.
         """
         ox.config(log_console=True, use_cache=True)
         self.graph = ox.graph_from_place(
-            place_full_name, network_type=movement_type
+            place_full_name, network_type=movement_type.value
         )
 
     def get_nearest_nodes(
@@ -60,13 +83,14 @@ class Graph:
         self,
         first_point: typing.Tuple[float, float],
         second_point: typing.Tuple[float, float],
-        optimizer: str = "length",
+        metrica_type: MetricaType = MetricaType.LENGTH,
     ) -> (int, typing.List[int]):
         # pylint: disable=unbalanced-tuple-unpacking
         """
         Возвращает кратчайший путь между двумя координатами в области графа.
 
-        Для точек first_point и second_point возьмется ближайший объект из графа.
+        Для точек first_point и second_point возьмется ближайший
+            объект из графа.
 
         Parameters
         ---------
@@ -74,28 +98,29 @@ class Graph:
             Координаты точки.
         second_point : typing.Tuple[float, float]
             Координаты точки.
-        optimizer : str {"length", "time"}
+        metrica_type : MetricaType
             Метрика, по которой ищем минимальный путь между двумя координатами.
 
         Returns
         ---------
         (distance, shortest_route) : (int, typing.List[int])
-            distance -
-                Если optimizer == "length": длина в метрах округленная до целых чисел.
-                Если optimizer == "time": время в часах, которое будет затрачено на маршрут.
-            shortest_route - путь от first_point до second_point в виде списка айдишников
-                             объектов из графа.
+            distance:
+                Если optimizer_type == MetricaType.LENGTH: длина в метрах
+                    округленная до целых чисел.
+                Если optimizer_type == MetricaType.TIME: время в часах,
+                    которое будет затрачено на маршрут.
+            shortest_route - путь от first_point до second_point в виде списка
+                айдишников объектов из графа.
         """
         first_node, second_node = self.get_nearest_nodes(
             [first_point, second_point]
         )
 
         shortest_route = nx.shortest_path(
-            self.graph, first_node, second_node, weight=optimizer
+            self.graph, first_node, second_node, weight=metrica_type.value
         )
         edge_lengths = utils_graph.get_route_edge_attributes(
-            self.graph, shortest_route, optimizer
+            self.graph, shortest_route, metrica_type.value
         )
-        distance = round(sum(edge_lengths))
 
-        return distance, shortest_route
+        return round(sum(edge_lengths)), shortest_route
